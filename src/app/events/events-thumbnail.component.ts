96a965357faf5de7d0102462e35d5682d2e26674
RyKizil <eray_kizilmese@hotmail.com>
2018-09-13 22:36:25 +0200

    added form

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { IEvent } from './shared/index';
 
 
 @Component({
@@ -22,6 +23,6 @@ import { Component, Input } from '@angular/core';
 })
 
 export class EventThumbnailComponent {
-    @Input() event: any
+    @Input() event: IEvent
     
 }
